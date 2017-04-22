95e250d834bac711b960a7f6589d6683888c2937
Kees C. Bakker <KeesCBakker@users.noreply.github.com>
2017-04-22 07:22:35 +0200

    Added some colors and echo more information.

--- install-node.sh
+++ install-node.sh
@@ -1,40 +1,70 @@
 #!/bin/bash
-# By Steven de Sales
+# By Kees C. Bakker / KeesTalksTech
 
-# 2017-04-20 Getting latest NodeJs version by Kees C. Bakker (KeesTalksTech)
+# Downloads NodeJS from the Node repository. Should work for any Pi. Will detect ARM version.
+# Tested with RPi1/armv6l and RPi3/armv7l.
+# Will remove a previous version installed by the script (up and downgrade compatible).
+
+# Syntax:
+# install-node.sh latest - will install latest node version
+# install-node.sh stable - will install the latest Long Term Stable (LTS) node version
+# install-node.sh {version} - will install that version.
+
+# Source: https://github.com/KeesCBakker/pi-config/blob/master/install-node.sh
+# Based on script by Kees C. Bakker @ https://github.com/KeesCBakker/node-pi-zero/blob/master/install-node-v.last.sh
+# Based on script by Steven de Salas @ https://github.com/sdesalas/node-pi-zero/blob/master/install-node-v7.7.1.sh
 # Based on script by Richard Stanley @ https://github.com/audstanley/Node-MongoDb-Pi/
+
+# colors
+W='\033[1;37m'
+G='\033[1;92m'
+R='\033[0m' # No Color
+
+echo ""
 
 #get pi ARM version
 PI_ARM_VERSION=$(
-  uname -a | 
-  egrep 'armv[0-9]+l' -o
+	uname -a | 
+	egrep 'armv[0-9]+l' -o
 );
 
 NODE_VERSION=$1
 
 if [ -z "$NODE_VERSION" ] || [ "$NODE_VERSION" = "stable" ]; then
-#get latest nodejs version from node website
-#read the first version that matches the arm platform
-NODE_VERSION=$(
-  curl https://nodejs.org/dist/index.json | 
-  egrep "{\"version\":\"v([0-9]+\.?){3}\"[^{]*\"linux-"$PI_ARM_VERSION"[^}]*lts\":\"[^}]*}" -o |
-  head -n 1
-);
+
+	echo -e "This script will${W} install the latest stable NodeJS (LTS)${R} for you."
+
+	#get latest nodejs stable version from node website
+	#read the first version that matches the arm platform and has a "lts":" part
+	NODE_VERSION=$(
+		curl -s https://nodejs.org/dist/index.json | 
+		egrep "{\"version\":\"v([0-9]+\.?){3}\"[^{}]*\"linux-"$PI_ARM_VERSION"[^}]*lts\":\"[^}]*}" -o |
+  		head -n 1
+	);
+
 elif [ "$NODE_VERSION" = "latest" ]; then
-NODE_VERSION=$(
-  curl https://nodejs.org/dist/index.json | 
-  egrep "{\"version\":\"v([0-9]+\.?){3}\"[^{]*\"linux-"$PI_ARM_VERSION"[^}]*}" -o |
-  head -n 1
+
+	echo -e "This script will ${W}install the latest NodeJS ${R}for you (non LTS included)."
+
+	#get latest nodejs stable version from node website
+	#read the first version that matches the arm platform
+	NODE_VERSION=$(
+  		curl -s https://nodejs.org/dist/index.json | 
+  		egrep "{\"version\":\"v([0-9]+\.?){3}\"[^{}]*\"linux-"$PI_ARM_VERSION"[^}]*}" -o |
+  		head -n 1
 );
 else 
-NODE_VERSION=$(
-  curl https://nodejs.org/dist/index.json | 
-  egrep "{\"version\":\"v$NODE_VERSION\"[^{]*\"linux-"$PI_ARM_VERSION"[^}]*}" -o |
-  head -n 1
-);
-fi
 
-echo "Node version: $NODE_VERSION"
+	echo -e "This script will try to ${W}install NodeJS v$NODE_VERSION ${R}for you."
+
+	#get specific nodejs version from node website
+	#make sure the version matches the arm platform
+	NODE_VERSION=$(
+  		curl -s https://nodejs.org/dist/index.json | 
+  		egrep "{\"version\":\"v$NODE_VERSION\"[^{}]*\"linux-"$PI_ARM_VERSION"[^}]*}" -o |
+  		head -n 1
+	);
+fi
 
 #get the version
 VERSION=$(
@@ -42,31 +72,43 @@ VERSION=$(
   egrep 'v([0-9]+\.?){3}' -o
 );
 
-echo "Version: $VERSION"
-
 #get lts version
 LTS_VERSION=$(echo $NODE_VERSION | 
-  egrep '"[^"]+"$' -o | 
+  egrep '"[^"]+"}$' -o | 
   egrep '[a-zA-Z]+' -o | 
   tr '[:upper:]' '[:lower:]'
 );
 
-echo "Lts version: $LTS_VERSION"
+#echo not found
+if [ -z "$VERSION" ]; then 	
+	echo -e "Version ${W}v$1${R} could not be found for architecture ${W}linux-$PI_ARM_VERSION${R}."
+	echo ""
+	exit 1
+fi
 
-# Creates directory for downloads, and downloads node
+#echo details before installing
+if [ -z "$LTS_VERSION" ] ; then
+	echo -e "NodeJS ${W}$VERSION${R} will be installed for architecture ${W}linux-$PI_ARM_VERSION${R}."
+	echo "This version is not LTS."
+else
+	echo -e "NodeJS ${W}$VERSION${R} ($LTS_VERSION) will be installed for architecture ${W}linux-$PI_ARM_VERSION${R}."
+fi
+
+# download to tmp directory
 cd /tmp/
 
 echo ""
-echo "Downloading:"
+echo -e "${W}Downloading${R}:"
 
 if [ -z "$LTS_VERSION" ]; then
-wget https://nodejs.org/dist/$VERSION/node-$VERSION-linux-$PI_ARM_VERSION.tar.gz;
+	wget https://nodejs.org/dist/$VERSION/node-$VERSION-linux-$PI_ARM_VERSION.tar.gz;
 else 
-wget https://nodejs.org/dist/latest-$LTS_VERSION/node-$VERSION-linux-$PI_ARM_VERSION.tar.gz;
+	wget https://nodejs.org/dist/latest-$LTS_VERSION/node-$VERSION-linux-$PI_ARM_VERSION.tar.gz;
 fi
 
-echo ""
-echo "Installing:"
+echo -e "${W}Installing${R}..."
+
+#tar the file
 tar -xzf node-$VERSION-linux-$PI_ARM_VERSION.tar.gz;
 
 # Remove the tar after extracing it.
@@ -99,11 +141,12 @@ sudo ln -s /opt/nodejs/bin/npm /sbin/npm;
 sudo ln -s /opt/nodejs/bin/npm /usr/local/bin/npm;
 
 echo ""
-echo "Installation ready!"
-echo ""
+echo -e "${G}Done${R}. Details:"
 
 NV=$(node -v)
-echo "Node version: $NV"
+echo -e "- Node version: ${W}$NV${R}"
 
 NPMV=$(npm -v)
-echo "NPM version: $NPMV"
+echo -e "- NPM version: ${W}$NPMV${R}"
+
+echo ""
