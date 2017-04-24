20529c9d8e989cbf3caf60522f03302e50ea82c6
Kees C. Bakker <KeesCBakker@users.noreply.github.com>
2017-04-24 22:55:17 +0200

    Create set-host-name.sh

---
+++ set-host-name.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# By Kees C. Bakker / KeesTalksTech
+
+# Changes the host name. Adds a line to the hosts file as well.
+
+# Syntax:
+# set-host-name.sh - will prompt for the hostname.
+# set-host-name.sh {name} - will make the given name the hostname
+
+# Source: https://github.com/KeesCBakker/pi-config/blob/master/set-host-name.sh
+
+# colors
+W='\033[1;37m';
+G='\033[1;92m';
+R='\033[0m' # No Color;
+
+HOSTNAME=$1;
+
+if [ -z "$HOSTNAME" ]; then
+	echo -e "Please enter a new host name: ${W}";
+	read HOSTNAME;
+fi
+
+#add to hostnname
+#prevents error message
+echo -e "127.0.1.1\t$HOSTNAME" | sudo tee --append /etc/hosts > /dev/null;
+
+#set hostname
+sudo hostnamectl set-hostname $HOSTNAME;
+
+#remove all 127.0.1.1 addresses
+cat /etc/hosts | 
+sed -n ':a;N;$!ba;s/127\.0\.1\.1..*$//gp' |
+sed -n '1h;1!H;${;g;s/$/127.0.1.1\t'$HOSTNAME'/gp}' |
+sudo tee /etc/hosts > /dev/null
+
+#capture new hostname
+HOSTNAME=$(hostname)
+
+#echo details
+echo ""
+echo -e "${R}Changed host name into ${W}$HOSTNAME${R}."
+echo -e "${R}Changed ${W}127.0.1.1${R} to ${W}$HOSTNAME${R} in ${W}/etc/hosts${R}"
+echo ""
