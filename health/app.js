cc1e848705eaad4b25ea293b378de16f5f5a2d95
Kees C. Bakker <c.bakker@gmx.net>
2017-04-28 18:56:29 +0200

    Added health app.

---
+++ health/app.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var app = require("health-app-base");
+
+app.default.start();
---
+++ health/install.sh
@@ -0,0 +1,28 @@
+
+set -e
+
+# colors
+W='\033[1;37m'
+G='\033[1;92m'
+R='\033[0m' # No Color
+
+#base url
+#BASE=https://raw.githubusercontent.com/KeesCBakker/pi-config/master/health
+
+#ensure app directory
+APPDIRECTORY=~/app/health
+if [ ! -d "$APPDIRECTORY" ]; then
+	mkdir APPDIRECTORY;
+fi
+
+#goto app directory
+cd $APPDIRECTORY
+
+
+npm install forever -g
+
+wget "$BASE/app.js" -O app.js
+wget "$BASE/app.json" -O app.json
+wget "$BASE/package.json" -O package.json
+
+npm update
